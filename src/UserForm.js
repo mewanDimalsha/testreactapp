1353681bb344032da91b0cc3b1dc807d4c9ac82d
Mewan Dimalsha <115539818+mewanDimalsha@users.noreply.github.com>
2024-05-14 13:59:02 +0530

    added user form and user compoents

---
+++ src/UserForm.js
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+const UserForm = () => {
+    return (
+        <Grid container spacing={2}
+        sx={}
+        >
+
+        </Grid>
+    );
+};
+
+export default UserForm;
